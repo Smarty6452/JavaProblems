871f50f0ae5e380db578fa9688508b1b98d03e6d
Rohit Bharti <rohit.21910374@viit.ac.in>
2022-01-19 15:04:13 +0530

    PatternProblemJava

---
+++ Patterns.java
@@ -0,0 +1,20 @@
+// package 01)Pattern;
+import java.util.*;
+
+public class Patterns {
+
+  
+
+    public static void main(String[] args) {
+        for(int i = 1 ; i <= 4 ; i++)
+        {
+            for(int j = 1 ; j <= 5 ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("*");
+            }
+        }
+    }
+    
+
